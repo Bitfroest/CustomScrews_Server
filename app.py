dbe1a52c4a6fd3d7debc8c169fd5e6fda876781a
Bitfroest <fabian.schurig.94@gmail.com>
2018-07-08 14:32:10 +0200

    added new server endpoints for saving screws

--- app.py
+++ app.py
@@ -1,25 +1,140 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import psycopg2
+from psycopg2.extras import RealDictCursor
 import config
-
-try:
-    conn = psycopg2.connect("dbname='"+config.database['dbname']+"' user='"+config.database['user']+"' host='"+config.database['host']+"' password='"+config.database['password']+"'")
-except:
-    print("I am unable to connect to the database")
-    
-try:
-    cur = conn.cursor()
-    cur.execute("""SELECT s.id,name,body_diameter,head_diameter,head_height,hexagon_diameter,hexagon_height,thread_length,body_length FROM iso_4762 as s LEFT OUTER JOIN iso_4762_length as l ON s.id = l.sid ORDER BY s.body_diameter ASC, l.body_length ASC""")
-    rows = cur.fetchall()
-except:
-    print("I can't drop our database!")
+from flask import g
 
 app = Flask(__name__)
+def connect_db():
+    try:
+        conn = psycopg2.connect("dbname='"+config.database['dbname']+"' user='"+config.database['user']+"' host='"+config.database['host']+"' port='"+config.database['port']+"' password='"+config.database['password']+"'")
+        return conn
+    except:
+        print("I am unable to connect to the database")
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'postgresql'):
+        g.postgresql = connect_db()
+    return g.postgresql
 
 @app.route('/', methods=['GET'])
 def get_tasks():
+    try:
+        cur = get_db().cursor()
+        cur.execute("""SELECT s.id,name,body_diameter,head_diameter,head_height,hexagon_diameter,hexagon_height,thread_length,body_length FROM iso_4762 as s LEFT OUTER JOIN iso_4762_length as l ON s.id = l.sid ORDER BY s.body_diameter ASC, l.body_length ASC""")
+        rows = cur.fetchall()
+    except:
+        print("I can't drop our database!")
     return jsonify({'iso_4762': rows})
+
+@app.route('/users/', methods = ['GET', 'POST'])
+def user():
+    if request.method == 'GET':
+        """return the information for users"""
+
+
+    if request.method == 'POST':
+        data = request.get_json()
+        if 'userId' in data:
+            sql = """INSERT INTO users(id, email, display_name, name) VALUES (%s,%s,%s,%s) ON CONFLICT("id") DO UPDATE SET email=%s ,display_name=%s,name=%s RETURNING id, email, display_name, name;"""
+            id = data.get("userId")
+            email = data.get("email", "")
+            display_name = data.get("display_name", "")
+            name = data.get("name", "")
+            cur = get_db().cursor(cursor_factory=RealDictCursor)
+            cur.execute(sql, (id,email,display_name,name,email,display_name,name,))
+            row = cur.fetchone()
+            get_db().commit()
+            cur.close()
+            return jsonify({'user': row})
+    else:
+        # POST Error 405 Method Not Allowed
+        return
+
+@app.route('/user/<id>/screw/<screw_id>', methods = ['GET', 'PUT'])
+def screw(id, screw_id):
+    if request.method == 'GET':
+        """return the information for users"""
+        cur = get_db().cursor(cursor_factory=RealDictCursor)
+        cur.execute("""SELECT s.id,name,body_diameter,head_diameter,head_height,hexagon_diameter,hexagon_height,thread_length,body_length FROM iso_4762 as s LEFT OUTER JOIN iso_4762_length as l ON s.id = l.sid WHERE s.id= %s AND (s.id_users=%s OR s.id_users IS NULL) ORDER BY s.body_diameter ASC, l.body_length ASC""", (screw_id,id,))
+        rows = cur.fetchall()
+        return jsonify({'iso_4762': rows})
+
+    if request.method == 'PUT':
+        data = request.get_json()
+        if 'name' in data and 'body_diameter' in data and 'head_diameter' in data and 'head_height' in data and 'hexagon_diameter' in data and 'hexagon_height' in data:
+            sql = """UPDATE iso_4762 SET (name, body_diameter, head_diameter, head_height, hexagon_diameter, hexagon_height) = (%s,%s,%s,%s,%s,%s) WHERE id=%s RETURNING *;"""
+            name = data.get("name")
+            body_diameter = data.get("body_diameter")
+            head_diameter = data.get("head_diameter")
+            head_height = data.get("head_height")
+            hexagon_diameter = data.get("hexagon_diameter")
+            hexagon_height = data.get("hexagon_height")
+            thread_length = data.get("thread_length")
+            body_length = data.get("body_length")
+            cur = get_db().cursor(cursor_factory=RealDictCursor)
+            cur.execute(sql, (name, body_diameter, head_diameter, head_height, hexagon_diameter, hexagon_height, screw_id,))
+            row = cur.fetchone()
+            get_db().commit()
+            cur.close()
+            return jsonify({'iso_4762': row})
+    else:
+        # POST Error 405 Method Not Allowed
+        return
+
+@app.route('/user/<id>/screws/', methods = ['GET', 'POST'])
+def screws(id):
+    if request.method == 'GET':
+        """return the information for users"""
+        cur = get_db().cursor(cursor_factory=RealDictCursor)
+        cur.execute("""SELECT s.id,name,body_diameter,head_diameter,head_height,hexagon_diameter,hexagon_height,thread_length,body_length FROM iso_4762 as s LEFT OUTER JOIN iso_4762_length as l ON s.id = l.sid WHERE s.id_users=%s OR s.id_users IS NULL ORDER BY s.body_diameter ASC, l.body_length ASC""", (id,))
+        rows = cur.fetchall()
+        return jsonify({'iso_4762': rows})
+
+    if request.method == 'POST':
+        data = request.get_json()
+        if 'name' in data and 'body_diameter' in data and 'head_diameter' in data and 'head_height' in data and 'hexagon_diameter' in data and 'hexagon_height' in data:
+            sql = """INSERT INTO iso_4762(name, body_diameter, head_diameter, head_height, hexagon_diameter, hexagon_height, id_users) VALUES (%s,%s,%s,%s,%s,%s,%s) RETURNING *;"""
+            name = data.get("name")
+            body_diameter = data.get("body_diameter")
+            head_diameter = data.get("head_diameter")
+            head_height = data.get("head_height")
+            hexagon_diameter = data.get("hexagon_diameter")
+            hexagon_height = data.get("hexagon_height")
+            thread_length = data.get("thread_length")
+            body_length = data.get("body_length")
+            cur = get_db().cursor(cursor_factory=RealDictCursor)
+            cur.execute(sql, (name, body_diameter, head_diameter, head_height, hexagon_diameter, hexagon_height, id,))
+            row = cur.fetchone()
+            get_db().commit()
+            cur.close()
+            return jsonify({'iso_4762': row})
+    else:
+        # POST Error 405 Method Not Allowed
+        return
+
+@app.route('/screw/<screw_id>/length/', methods = ['GET', 'POST'])
+def length(screw_id):
+    if request.method == 'POST':
+        data = request.get_json()
+        if 'thread_length' in data and 'body_length' in data:
+            sql = """INSERT INTO iso_4762_length(thread_length, body_length, sid) SELECT %s, %s, %s WHERE EXISTS (SELECT 1 FROM iso_4762 WHERE id=%s) RETURNING *;"""
+            thread_length = data.get("thread_length")
+            body_length = data.get("body_length")
+            cur = get_db().cursor(cursor_factory=RealDictCursor)
+            cur.execute(sql, (thread_length,body_length,screw_id,screw_id,))
+            row = cur.fetchone()
+            get_db().commit()
+            cur.close()
+            return jsonify({'iso_4762_length': row})
+    else:
+        # POST Error 405 Method Not Allowed
+        return
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
--- createdb.sql
+++ createdb.sql
@@ -1,34 +1,109 @@
--- Database: screws
+-- Database generated with pgModeler (PostgreSQL Database Modeler).
+-- pgModeler  version: 0.9.1-beta
+-- PostgreSQL version: 10.0
+-- Project Site: pgmodeler.io
+-- Model Author: ---
 
--- DROP DATABASE screws;
 
-CREATE DATABASE screws
-  WITH OWNER = postgres
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       LC_COLLATE = 'German_Germany.1252'
-       LC_CTYPE = 'German_Germany.1252'
-       CONNECTION LIMIT = -1;
+-- Database creation must be done outside an multicommand file.
+-- These commands were put in this file only for convenience.
+-- -- object: screws | type: DATABASE --
+-- -- DROP DATABASE IF EXISTS screws;
+-- CREATE DATABASE screws
+-- 	ENCODING = 'UTF8'
+-- 	LC_COLLATE = 'German_Germany.1252'
+-- 	LC_CTYPE = 'German_Germany.1252'
+-- 	TABLESPACE = pg_default
+-- 	OWNER = postgres
+-- ;
+-- -- ddl-end --
+-- 
 
--- Table: public.iso_4762
+-- object: public.iso_id_seq | type: SEQUENCE --
+-- DROP SEQUENCE IF EXISTS public.iso_id_seq CASCADE;
+CREATE SEQUENCE public.iso_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START WITH 1
+	CACHE 1
+	NO CYCLE
+	OWNED BY NONE;
+-- ddl-end --
+ALTER SEQUENCE public.iso_id_seq OWNER TO postgres;
+-- ddl-end --
 
--- DROP TABLE public.iso_4762;
+-- object: public.length_id_seq | type: SEQUENCE --
+-- DROP SEQUENCE IF EXISTS public.length_id_seq CASCADE;
+CREATE SEQUENCE public.length_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START WITH 1
+	CACHE 1
+	NO CYCLE
+	OWNED BY NONE;
+-- ddl-end --
+ALTER SEQUENCE public.length_id_seq OWNER TO postgres;
+-- ddl-end --
 
-CREATE TABLE public.iso_4762
-(
-  id integer NOT NULL,
-  name text,
-  body_diameter real,
-  head_diameter real,
-  head_height real,
-  hexagon_diameter real,
-  hexagon_height real,
-  thread_length real,
-  body_length real,
-  CONSTRAINT id PRIMARY KEY (id)
-)
-WITH (
-  OIDS=FALSE
+-- object: public.iso_4762 | type: TABLE --
+-- DROP TABLE IF EXISTS public.iso_4762 CASCADE;
+CREATE TABLE public.iso_4762(
+	id integer NOT NULL DEFAULT nextval('public.iso_id_seq'::regclass),
+	name text,
+	body_diameter real,
+	head_diameter real,
+	head_height real,
+	hexagon_diameter real,
+	hexagon_height real,
+	created timestamp,
+	user_id_user text,
+	CONSTRAINT id PRIMARY KEY (id)
+
 );
-ALTER TABLE public.iso_4762
-  OWNER TO postgres;
+-- ddl-end --
+ALTER TABLE public.iso_4762 OWNER TO postgres;
+-- ddl-end --
+
+-- object: public.iso_4762_length | type: TABLE --
+-- DROP TABLE IF EXISTS public.iso_4762_length CASCADE;
+CREATE TABLE public.iso_4762_length(
+	id integer NOT NULL DEFAULT nextval('public.length_id_seq'::regclass),
+	thread_length real,
+	body_length real,
+	sid integer NOT NULL,
+	CONSTRAINT iso_4762_length_pkey PRIMARY KEY (id)
+
+);
+-- ddl-end --
+ALTER TABLE public.iso_4762_length OWNER TO postgres;
+-- ddl-end --
+
+-- object: public."user" | type: TABLE --
+-- DROP TABLE IF EXISTS public."user" CASCADE;
+CREATE TABLE public."user"(
+	id text NOT NULL,
+	email text,
+	display_name text,
+	name text,
+	CONSTRAINT user_pk PRIMARY KEY (id)
+
+);
+-- ddl-end --
+
+-- object: user_fk | type: CONSTRAINT --
+-- ALTER TABLE public.iso_4762 DROP CONSTRAINT IF EXISTS user_fk CASCADE;
+ALTER TABLE public.iso_4762 ADD CONSTRAINT user_fk FOREIGN KEY (user_id_user)
+REFERENCES public."user" (id) MATCH FULL
+ON DELETE SET NULL ON UPDATE CASCADE;
+-- ddl-end --
+
+-- object: screwfk | type: CONSTRAINT --
+-- ALTER TABLE public.iso_4762_length DROP CONSTRAINT IF EXISTS screwfk CASCADE;
+ALTER TABLE public.iso_4762_length ADD CONSTRAINT screwfk FOREIGN KEY (sid)
+REFERENCES public.iso_4762 (id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
+
+
